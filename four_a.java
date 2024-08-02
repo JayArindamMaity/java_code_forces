f56c72bf93c22a7a716a24c87f34530cfaa9b63e
JayArindamMaity <maityjayarindam@gmail.com>
2024-08-02 10:02:21 +0530

    problem 4A

---
+++ four_a.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class learn {
+  public static void main (String [] args) {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    if (num%2 != 0) {
+      System.out.println("NO");
+    }
+    else if ((num/2)%2==0) {
+      System.out.println("YES");
+    }
+    else {
+      System.out.println("NO");
+    }
+    sc.close();
+  }
+}
