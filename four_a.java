116a7e1f13d9eee444b5110b93265ff61fee03ce
JayArindamMaity <maityjayarindam@gmail.com>
2024-08-05 11:18:20 +0530

    one_a

--- four_a.java
+++ four_a.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class learn {
+public class four_a {
   public static void main (String [] args) {
     Scanner sc = new Scanner(System.in);
     int num = sc.nextInt();
---
+++ one_a.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class one_a {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger row = sc.nextBigInteger();
+        BigInteger col = sc.nextBigInteger();
+        BigInteger til = sc.nextBigInteger();
+            
+        BigInteger len = row.divide(til).add(row.mod(til).equals(BigInteger.ZERO) ? BigInteger.ZERO : BigInteger.ONE);
+        BigInteger bre = col.divide(til).add(col.mod(til).equals(BigInteger.ZERO) ? BigInteger.ZERO : BigInteger.ONE);
+            
+        System.out.println(len.multiply(bre));
+        sc.close();
+    }
+}
