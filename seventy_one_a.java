06f4db478cd6b9edd309e70e19e6274fa848acfe
JayArindamMaity <maityjayarindam@gmail.com>
2024-08-06 11:46:36 +0530

    seventy_one_a

---
+++ seventy_one_a.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class seventy_one_a {
+  public static void main(String [] args){
+    java.util.Scanner sc = new Scanner(System.in);
+    int runner;
+    runner = sc.nextInt();
+    while (runner >= 0){
+      String usr_inp = sc.nextLine();
+      System.out.println((usr_inp.length()<=10) ? usr_inp : (usr_inp.charAt(0) + "" + Integer.toString(usr_inp.length() - 2)  +"" + usr_inp.charAt(usr_inp.length() - 1)));
+      runner--;
+    }
+    sc.close();
+  }
+}
